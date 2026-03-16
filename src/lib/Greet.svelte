640db037679feb1d10fa1abff1543b5aa4c1dcb3
willbraun <williamhbraun1@gmail.com>
2026-03-16 09:20:23 -0400

    first commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { invoke } from '@tauri-apps/api/core';
+
+	let name = $state('');
+	let greetMsg = $state('');
+
+	async function greet() {
+		greetMsg = await invoke('greet', { name });
+	}
+</script>
+
+<div>
+	<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+	<button onclick={greet}>Greet</button>
+	<p>{greetMsg}</p>
+</div>
